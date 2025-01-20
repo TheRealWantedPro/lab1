ccc22d1425b055b0d5b6b5b6573597e18af72189
John Doe <johndoe@example.com>
2025-01-20 10:53:05 -0500

    Added Two Features

--- lab02.py
+++ lab02.py
@@ -5,23 +5,27 @@ username = input("Enter your Username: ")
 password = input("Enter your StudentNumber: ")
 
 print("Choose an option:")
-print("1. Read a file")
-print("2. Write to a file")
+print("1. Display Product Inventory")
+print("2. Add to Product Inventory")
 choice = input("Enter 1 or 2: ")
 
-def readfile():
-    with open('inventory_data.csv', mode='r', newline='') as file:
-        csvFile = csv.reader(file)
-        for row in csvFile:
-            print(",".join(row))  
-
-
-def writefile():
-    with open('inventory_data.csv', mode='w', newline='') as file:
-        input = csv.writer(file)
-    input.writerows(data)
+if choice == "1":
+    try:
+        with open('inventory_data.csv', mode='r', newline='') as file:
+            csvFile = csv.reader(file)
+            for row in csvFile:
+                print(",".join(row))
+    except FileNotFoundError:
+        print("Error: File not found.")
+elif choice == "2":
+    content = input("Enter Product: ")
+    with open('inventory_data.csv', 'w') as file:
+        file.write(content)
+    print("Product Added  to file successfully.")
+else:
+    print("Invalid choice. Please enter 1 or 2.")
 
 
 data = pd.read_csv('inventory_data.csv')
 data_dict = data.to_dict(orient='records')
-print(readfile())
+
